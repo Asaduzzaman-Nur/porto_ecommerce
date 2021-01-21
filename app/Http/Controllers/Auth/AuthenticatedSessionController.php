430a4bf955c254a08d0e88ca333c3511125e0feb
Asaduzzaman-Nur <67062897+Asaduzzaman-Nur@users.noreply.github.com>
2021-01-21 21:59:21 +0600

    UserSeeder

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -49,6 +49,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('staff/login');
+        return redirect('/staff/login');
     }
 }
